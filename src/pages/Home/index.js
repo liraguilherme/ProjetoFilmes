ce8f8182d45c98fdbbf1587a134a789788e6bd72
realkodesh <liraguilherme186@gmail.com>
2024-10-23 22:21:07 -0300

    Listing movies

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState} from 'react';
 import api from '../../services/api';
-
+import {Link} from 'react-router-dom'
+import './home.css';
 //UseEffect assim que abrir a página vai na api e busca os filmes
 //useState após a busca armazenar em algum estado para usar na aplicação
 //URL DA API: /movie/now_playing?api_key=fe87f11073175e006baff35e3b136a01&language=pt-BR
@@ -21,8 +22,10 @@ useEffect(() =>{
 
             }
         })
+        
 
-        console.log(response.data.results);
+        //console.log(response.data.results.slice(0, 10));
+        setFilmes(response.data.results.slice(0, 10))
         
     }
 
@@ -32,8 +35,20 @@ useEffect(() =>{
 }, [])
 
     return(
-        <div>
-            <h1>BEM VINDO A HOME</h1>
+        <div className="container">
+            <div className="lista-filmes">
+            
+                {filmes.map((filme) => {
+                    return(
+                        <article key={filme.id}>
+                            <strong>{filme.title}</strong>
+                            <img src={`https://image.tmdb.org/t/p/original/${filme.poster_path}`} alt={filme.title} />
+                            <Link to={`/filme/${filme.id}`}>Acessar</Link>
+                        </article>
+                    )
+                })}
+            </div>
+  
         </div>
     )
 }
