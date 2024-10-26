5d725cd6f8c11c3ba9e245beae199a8ff4415616
realkodesh <liraguilherme186@gmail.com>
2024-10-25 21:23:27 -0300

    Adding movie details

--- src/pages/Filme/index.js
+++ src/pages/Filme/index.js
@@ -1,9 +1,56 @@
 // Rotas
 
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import api from '../../services/api';
+
 function Filme(){
+    const { id } =  useParams();
+
+    const [filme, setFilme] = useState({});
+    const [loading, setLoading] = useState(true); 
+
+    useEffect(() =>{
+      async function loadFilme() {
+        await api.get(`/movie/${id}`, {
+            params:{
+                api_key:"fe87f11073175e006baff35e3b136a01",
+                language: "pt-BR",
+            }
+
+        })
+        .then((response) =>{ //Caso de sucesso
+          setFilme(response.data);
+          setLoading(false);
+        })
+        .catch(() => {
+            console.log("FILME NÃO ENCONTRADO")
+        })
+      }
+
+      loadFilme();
+
+      return () => {
+        console.log("COMPONENTE FOI DESMONTADO")
+      }
+    }, [])
+
+    if(loading){
+      return(
+        <div className="filme-info">
+          <h1>Carregando detalhes...</h1>
+        </div>
+      )
+    }
+
     return(
-        <div>
-            <h1>BEM VINDO A PAGINA DETALHES DO FILME</h1>
+        <div className="filme-info">
+          <h1>{filme.title}</h1>
+          <img src={`https://image.tmdb.org/t/p/original/${filme.backdrop_path}`} alt={filme.title} />
+          <h3>Sinopse</h3>
+          <span>{filme.overview}</span>
+
+          <strong>Avaliação: {filme.vote_average} / 10</strong>
         </div>
     )
 }
